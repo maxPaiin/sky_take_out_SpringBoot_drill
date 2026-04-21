3b4a4381293f7df4f4cfdfbd6c5ddb70e613077f
kojima <192161418+maxPaiin@users.noreply.github.com>
2026-04-22 07:21:23 +0900

    修正了jdk版本

--- sky-server/src/main/java/com/sky/config/OssConifguration.java
+++ sky-server/src/main/java/com/sky/config/OssConifguration.java
@@ -4,7 +4,6 @@ import com.sky.properties.AliOssProperties;
 import com.sky.utils.AliOssUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
